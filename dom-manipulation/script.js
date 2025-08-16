03d51237f853a7bbb14aa0c98cf169cb8d545822
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-08-16 14:12:13 +0300

    initial commit

---
+++ dom-manipulation/script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const quoteText = document.getElementById('newQuoteText')
+    const quoteCategory = document.getElementById('newQuoteCategory')
+    const quotes = [
+        {
+            text: "The best way to get started is to quit talking and begin doing.",
+            category: "Motivation"
+        },
+        {
+            text: "Your time is limited, so don’t waste it living someone else’s life.",
+            category: "Inspiration"
+        },
+        // {
+        //     text: "In the middle of every difficulty lies opportunity.",
+        //     category: "Wisdom"
+        // },
+        // {
+        //     text: "Happiness depends upon ourselves.",
+        //     category: "Philosophy"
+        // },
+        // {
+        //     text: "Do not let what you cannot do interfere with what you can do.",
+        //     category: "Encouragement"
+        // }
+    ];
+
+    function showRandomQuote() {
+
+    }
+
+    function createAddQuoteForm() {
+
+    }
+
+    function addQuote() {
+
+    }
+
+})
