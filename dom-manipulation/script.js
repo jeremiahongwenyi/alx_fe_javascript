0164e01ebe60e338644c64804de76111f5087062
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-08-16 21:52:44 +0300

    implement import and export

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -4,11 +4,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const quoteDisplay = document.getElementById('quoteDisplay');
     const newQuoteBtn = document.getElementById('newQuote');
     const addQuoteBtn = document.getElementById('addQuote');
-    const quotes = [
-        { text: "The best way to get started is to quit talking and begin doing.", category: "Motivation" },
-        { text: "Your time is limited, so don’t waste it living someone else’s life.", category: "Inspiration" },
-        { text: "In the middle of every difficulty lies opportunity.", category: "Wisdom" }
-    ];
+    const importFile = document.getElementById('importFile');
+    const exportJson = document.getElementById('exportJson')
+
+
+
+    let quotes = JSON.parse(localStorage.getItem('quotes')) || []
+    console.log(quotes);
+
+
+    // const quotes = [
+    //     { text: "The best way to get started is to quit talking and begin doing.", category: "Motivation" },
+    //     { text: "Your time is limited, so don’t waste it living someone else’s life.", category: "Inspiration" },
+    //     { text: "In the middle of every difficulty lies opportunity.", category: "Wisdom" }
+    // ];
 
     function showRandomQuote() {
         if (!quotes.length > 0) {
@@ -20,43 +29,66 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(index);
         quoteDisplay.innerText = quotes[index].text
 
+        sessionStorage.setItem('lastViewedItem', JSON.stringify(quotes[index].text))
+
     }
 
 
     function createAddQuoteForm() {
         if (quoteText.value === '' || quoteCategory.value === '') {
             console.log('no value typed');
-
             return;
         }
         const data = { text: quoteText.value, category: quoteCategory.value }
         quotes.push(data)
         console.log(quotes);
+        saveQuotes()
         const paragraph = document.createElement('p')
         paragraph.innerText = quoteText.value
         quoteDisplay.appendChild(paragraph)
 
     }
 
+    function importFromJsonFile(event) {
+        const fileReader = new FileReader();
+        fileReader.onload = function (event) {
+            const importedQuotes = JSON.parse(event.target.result);
+            console.log('this are my imported quotes', (importedQuotes));
+
+            quotes.push(...importedQuotes);
+            saveQuotes();
+            alert('Quotes imported successfully!');
+        };
+        fileReader.readAsText(event.target.files[0]);
+    }
+
+    function saveQuotes() {
+        localStorage.setItem('quotes', JSON.stringify(quotes))
+    }
+
+    function exportToJsonFile() {
+        const dataStr = JSON.stringify(quotes, null, 2);
+        const blob = new Blob([dataStr], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'quotes.json';
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+        URL.revokeObjectURL(url);
+    }
+
+
 
 
     // Show a random quote on button click
     newQuoteBtn.addEventListener('click', showRandomQuote);
     addQuoteBtn.addEventListener('click', createAddQuoteForm)
+    importFile.addEventListener('change', importFromJsonFile)
+    exportJson.addEventListener('click', exportToJsonFile)
 
-    // Optionally show one on load
-    // showRandomQuote();
+
 
 })
-
-
-//    if (quotes.length === 0) {
-//             quoteDisplay.innerHTML = "<em>No quotes yet. Add one below.</em>";
-//             return;
-//         }
-//         const index = Math.floor(Math.random() * quotes.length); // uses 'random'
-//         const q = quotes[index];
-//         quoteDisplay.innerHTML = `
-//       <blockquote>${q.text}</blockquote>
-//       <small>${q.category}</small>
-//     `; // uses 'innerHTML'
