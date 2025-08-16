ea16be3f40f4a7684f0691ca268677480adf062c
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-08-16 20:44:54 +0300

    add add note logic

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const quoteCategory = document.getElementById('newQuoteCategory');
     const quoteDisplay = document.getElementById('quoteDisplay');
     const newQuoteBtn = document.getElementById('newQuote');
+    const addQuoteBtn = document.getElementById('addQuote');
     const quotes = [
         { text: "The best way to get started is to quit talking and begin doing.", category: "Motivation" },
         { text: "Your time is limited, so don’t waste it living someone else’s life.", category: "Inspiration" },
@@ -10,30 +11,50 @@ document.addEventListener('DOMContentLoaded', () => {
     ];
 
     function showRandomQuote() {
-        if (quotes.length === 0) {
-            quoteDisplay.innerHTML = "<em>No quotes yet. Add one below.</em>";
+        if (!quotes.length > 0) {
+            // const paragraph = document.createElement('p')
+            // paragraph
+            quoteDisplay.innerText = "There are no quotes yet,  add one below."
             return;
         }
-        const index = Math.floor(Math.random() * quotes.length); // uses 'random'
-        const q = quotes[index];
-        quoteDisplay.innerHTML = `
-      <blockquote>${q.text}</blockquote>
-      <small>${q.category}</small>
-    `; // uses 'innerHTML'
+
+        let index = Math.floor(Math.random() * quotes.length)
+        console.log(index);
+        quoteDisplay.innerText = quotes[index].text
+
     }
+
 
     function createAddQuoteForm() {
-        // we'll fill this later
+        if (quoteText.value === '' || quoteCategory.value === '') {
+            console.log('no value typed');
+
+            return;
+        }
+        const data = { text: quoteText.value, category: quoteCategory.value }
+        quotes.push(data)
+        console.log(quotes);
     }
 
-    function addQuote() {
-        // we'll fill this later
-    }
+
 
     // Show a random quote on button click
-    newQuoteBtn.addEventListener('click', displayRandomQuote);
+    newQuoteBtn.addEventListener('click', showRandomQuote);
+    addQuoteBtn.addEventListener('click', createAddQuoteForm)
 
     // Optionally show one on load
-    showRandomQuote();
+    // showRandomQuote();
 
 })
+
+
+//    if (quotes.length === 0) {
+//             quoteDisplay.innerHTML = "<em>No quotes yet. Add one below.</em>";
+//             return;
+//         }
+//         const index = Math.floor(Math.random() * quotes.length); // uses 'random'
+//         const q = quotes[index];
+//         quoteDisplay.innerHTML = `
+//       <blockquote>${q.text}</blockquote>
+//       <small>${q.category}</small>
+//     `; // uses 'innerHTML'
